5171621f3970597308a8383dfc8fef6bb1252169
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2017-02-17 22:03:16 +0100

    Finished Title-Bar

--- VisualStudioShell/MainPage.xaml.cs
+++ VisualStudioShell/MainPage.xaml.cs
@@ -1,5 +1,9 @@
-﻿using Windows.UI;
+﻿using System;
+using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.Core;
 using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 
@@ -10,14 +14,30 @@ namespace VisualStudioShell
     public MainPage()
     {
       this.InitializeComponent();
+      CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = false;
+      SetTitleBar();
+      StyleTitleBar();
+    }
+
+    private void SetTitleBar()
+    {
+      var titleBar = CoreApplication.GetCurrentView().TitleBar;
+      titleBar.ExtendViewIntoTitleBar = true;
+
+      Window.Current.SetTitleBar(backgroundElement);
+    }
+
+    private static void StyleTitleBar()
+    {
       var brush = (SolidColorBrush)App.Current.Resources["VsMainBackground"];
-      var titleBar = ApplicationView.GetForCurrentView().TitleBar;
-      if (titleBar != null)
+      var appView = ApplicationView.GetForCurrentView();
+      var appViewTitleBar = appView.TitleBar;
+      if (appViewTitleBar != null)
       {
-        titleBar.ButtonBackgroundColor = brush.Color;
-        titleBar.ButtonForegroundColor = Colors.White;
-        titleBar.BackgroundColor = brush.Color;
-        titleBar.ForegroundColor = Colors.White;
+        appViewTitleBar.ButtonBackgroundColor = brush.Color;
+        appViewTitleBar.ButtonForegroundColor = Colors.White;
+        appViewTitleBar.BackgroundColor = brush.Color;
+        appViewTitleBar.ForegroundColor = Colors.White;
       }
     }
   }
