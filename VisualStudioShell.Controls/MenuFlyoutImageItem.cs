dca2f62eb2d02ed857403cf933e075829f3214a2
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2017-02-17 16:08:13 +0100

    Initial Commit

---
+++ VisualStudioShell.Controls/MenuFlyoutImageItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace VisualStudioShell.Controls
+{
+  public sealed class MenuFlyoutImageItem : MenuFlyoutItem
+  {
+    public MenuFlyoutImageItem()
+    {
+      this.DefaultStyleKey = typeof(MenuFlyoutImageItem);
+    }
+
+    public ImageSource Icon
+    {
+      get { return (ImageSource)GetValue(IconProperty); }
+      set { SetValue(IconProperty, value); }
+    }
+
+    public static readonly DependencyProperty IconProperty =
+        DependencyProperty.Register("Icon", typeof(ImageSource), typeof(MenuFlyoutImageItem), new PropertyMetadata(null));
+
+
+  }
+}
---
+++ VisualStudioShell/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace VisualStudioShell
+{
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
+    {
+      this.InitializeComponent();
+      var brush = (SolidColorBrush)this.Resources["VsMainBackground"];
+      var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+      if (titleBar != null)
+      {
+        titleBar.ButtonBackgroundColor = brush.Color;
+        titleBar.ButtonForegroundColor = Colors.White;
+        titleBar.BackgroundColor = brush.Color;
+        titleBar.ForegroundColor = Colors.White;
+      }
+    }
+  }
+}
