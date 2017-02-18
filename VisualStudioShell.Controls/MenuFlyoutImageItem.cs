a41703b23c57a67ababbb8a9c01f84f2e1991b7b
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2017-02-18 22:21:41 +0100

    Clean up MenuFlyoutImageItem

--- VisualStudioShell.Controls/MenuFlyoutImageItem.cs
+++ VisualStudioShell.Controls/MenuFlyoutImageItem.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Documents;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-
-// The Templated Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234235
 
 namespace VisualStudioShell.Controls
 {
@@ -28,7 +19,5 @@ namespace VisualStudioShell.Controls
 
     public static readonly DependencyProperty IconProperty =
         DependencyProperty.Register("Icon", typeof(ImageSource), typeof(MenuFlyoutImageItem), new PropertyMetadata(null));
-
-
   }
 }
